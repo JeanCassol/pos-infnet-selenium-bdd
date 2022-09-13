fc5b37475683ba60c82652f1afc2d6e8f2004c77
Jean Cassol <jean_cassol@yahoo.com.br>
2022-09-12 21:05:24 -0300

    feat: create project

---
+++ src/main/java/com/pos/posinfnetseleniumbdd/PosInfnetSeleniumBddApplication.java
@@ -0,0 +1,13 @@
+package com.pos.posinfnetseleniumbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosInfnetSeleniumBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosInfnetSeleniumBddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pos/posinfnetseleniumbdd/PosInfnetSeleniumBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.posinfnetseleniumbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosInfnetSeleniumBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
